3e60c833cfea7775e98393af7ba750843833d6c5
shenrique08 <114961377+shenrique08@users.noreply.github.com>
2022-10-26 19:36:59 -0300

    Update ex002.c

--- Lab03/ex002.c
+++ Lab03/ex002.c
@@ -4,12 +4,14 @@
 int main()
 {
     int n1;
+    
     printf("Type an integer: ");
     scanf("%d", &n1);
-    if (n1 > 0){
+    
+    if (n1 > 0) {
         printf("The square root of %d is %d", n1, (n1*n1));
 
-    }else{
+    } else {
         printf("%d is invalid ", n1);
         
     }

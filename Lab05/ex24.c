e782918a213bbcf9ff43c7b1b756876cfa838ab3
shenrique08 <114961377+shenrique08@users.noreply.github.com>
2022-11-21 17:57:47 -0300

    Update ex24.c

--- Lab05/ex24.c
+++ Lab05/ex24.c
@@ -7,7 +7,7 @@ void desenho_triangulo(int n)
         for (int j = 0; j < n - i - 1; j++) {
             printf(" ");
         }
-        for (int k = 0; k < 2*i + 1; k++) {
+        for (int k = 0; k < 2 * i + 1; k++) {
             printf("*");
         }
         printf("\n");
